d02b6d3350081338d5a566ba81b672a3ae17a9bc
subin4256 <subin4256@naver.com>
2021-10-26 18:46:18 +0900

    06

--- src/project1/PhoneBookVer06.java
+++ src/project1/PhoneBookVer06.java
@@ -1,10 +1,12 @@
 package project1;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-import project1.ver05.MenuItem;
-import project1.ver05.PhoneBookManager;
-import project1.ver05.PhoneInfo;
+import project1.ver06.MenuItem;
+import project1.ver06.PhoneBookManager;
+import project1.ver06.PhoneInfo;
+import project1.ver06.MenuSelectException;
 
 public class PhoneBookVer06
 {
@@ -22,39 +24,60 @@ public class PhoneBookVer06
 		
 		Scanner scanner = new Scanner(System.in);
 		PhoneBookManager pbm = new PhoneBookManager(100);
-		
+
 		while(true) {
-			
-			printMenu();
-			
-			int choice = scanner.nextInt();
-			switch(choice) {
-			case MenuItem.INPUT: 
-				//데이터 입력
-				pbm.dataInput(choice);
-				System.out.println();
-				break;
-			case MenuItem.SEARCH:
-				//데이터 검색
-				pbm.dataSearch(choice);
-				System.out.println();
-				break;
-			case MenuItem.DELETE:
-				//데이터 삭제
-				pbm.dataDelete(choice);
-				System.out.println();
-				break;
-			case MenuItem.SHOW:
-				//주소록 출력
-				pbm.dataAllShow(choice);
-				System.out.println();
-				break;
-			case MenuItem.TERMINATE:
-				//프로그램 종료
-				System.out.println("프로그램종료");
-				return;
+			try {
+				
+				printMenu();
+				
+				int choice = scanner.nextInt();
+				if(choice<1 || choice>5) {
+					MenuSelectException mse = new MenuSelectException();
+					throw mse;
+				}
+				
+				switch(choice) {
+				case MenuItem.INPUT: 
+					//데이터 입력
+					pbm.dataInput(choice);
+					System.out.println();
+					break;
+				case MenuItem.SEARCH:
+					//데이터 검색
+					pbm.dataSearch(choice);
+					System.out.println();
+					break;
+				case MenuItem.DELETE:
+					//데이터 삭제
+					pbm.dataDelete(choice);
+					System.out.println();
+					break;
+				case MenuItem.SHOW:
+					//주소록 출력
+					pbm.dataAllShow(choice);
+					System.out.println();
+					break;
+				case MenuItem.TERMINATE:
+					//프로그램 종료
+					System.out.println("프로그램종료");
+					return;
+				}
+				
 			}
-			
+			catch(MenuSelectException e) {
+				e.printStackTrace();
+			}
+			catch (InputMismatchException e) {
+				System.out.println("숫자만 입력해주세요.");
+				e.printStackTrace();
+			}
+			catch(NullPointerException e) {
+				System.out.println("검색결과가 없습니다.");
+			}
+			catch(Exception e) {
+				System.out.println("알 수 없는 오류 발생");
+				e.printStackTrace();
+			}
 		}
 		
 	}
